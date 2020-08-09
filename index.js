0022a4b74fc79047f0c1e8cad68e0cc4f474a1f4
jordanoffshl <69113217+jordanoffshl@users.noreply.github.com>
2020-08-08 20:26:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const client = new Discord.Client();
 const prefix = "^^";
 client.on("ready", message => {
     console.log('bot => ok')
-    
-    
-    })
+})
     
     client.on("message", message => {
     if(message.content === `${prefix}ping`) {
         message.channel.send(`🏓 pong - ${client.ws.ping}`)
     }
     
-    client.login(process.env.TOKEN)
+    if(message.content === `^^help`)
+        message.channel.send(`salut je m'appelle koya`)
+    }
+ })
+client.login(process.env.TOKEN);
